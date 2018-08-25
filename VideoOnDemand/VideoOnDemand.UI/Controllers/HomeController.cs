0e6a42cc5692679a5132e0b6fdd6e5327d139ef8
Minh Lam <kelvincoder@gmail.com>
2018-08-25 17:17:04 +0700

    Styling Login Form

--- VideoOnDemand/VideoOnDemand.UI/Controllers/HomeController.cs
+++ VideoOnDemand/VideoOnDemand.UI/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using VideoOnDemand.Data.Data.Entities;
 using VideoOnDemand.UI.Models;
 
 namespace VideoOnDemand.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private SignInManager<User> _signInManager;
+
+        public HomeController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Login", "Account");
+            }
             return View();
         }
 
--- VideoOnDemand/VideoOnDemand.UI/Startup.cs
+++ VideoOnDemand/VideoOnDemand.UI/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using VideoOnDemand.UI.Data;
-using VideoOnDemand.UI.Models;
 using VideoOnDemand.UI.Services;
+using VideoOnDemand.Data.Data;
+using VideoOnDemand.Data.Data.Entities;
 
 namespace VideoOnDemand.UI
 {
@@ -26,11 +22,11 @@ namespace VideoOnDemand.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<VODContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<VODContext>()
                 .AddDefaultTokenProviders();
 
             // Add application services.
