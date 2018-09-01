80de039ac3c8ef01ed81bb1052243e420af683f0
Minh Lam <kelvincoder@gmail.com>
2018-09-01 13:40:04 +0700

    Configure AutoMapper

---
+++ VideoOnDemand/VideoOnDemand.UI/Controllers/MembershipController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VideoOnDemand.Data.Data.Entities;
+using VideoOnDemand.Data.Repositories;
+
+namespace VideoOnDemand.UI.Controllers
+{
+    public class MembershipController : Controller
+    {
+        private string _userId;
+        private IMapper _mapper;
+        private IReadRepository _readRepository;
+
+        public MembershipController(IHttpContextAccessor httpContextAcessor, 
+            UserManager<User> userManager, 
+            IMapper mapper,
+            IReadRepository readRepository)
+        {
+            var user = httpContextAcessor.HttpContext.User;
+            _userId = userManager.GetUserId(user);
+            _mapper = mapper;
+            _readRepository = readRepository;
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Course(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Video(int id)
+        {
+            return View();
+        }
+    }
+}
--- VideoOnDemand/VideoOnDemand.UI/Startup.cs
+++ VideoOnDemand/VideoOnDemand.UI/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using VideoOnDemand.UI.Services;
 using VideoOnDemand.Data.Data;
 using VideoOnDemand.Data.Data.Entities;
+using VideoOnDemand.Data.Repositories;
+using VideoOnDemand.UI.Models.DTOModels;
 
 namespace VideoOnDemand.UI
 {
@@ -31,8 +33,30 @@ namespace VideoOnDemand.UI
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
+            services.AddSingleton<IReadRepository, MockReadRepository>();
 
             services.AddMvc();
+
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Video, VideoDTO>();
+                cfg.CreateMap<Download, DownloadDTO>()
+                    .ForMember(des => des.DownloadTitle, src => src.MapFrom(s => s.Title))
+                    .ForMember(des => des.DownloadUrl, src => src.MapFrom(s => s.Url));
+                cfg.CreateMap<Instructor, InstructorDTO>()
+                    .ForMember(des => des.InstructorName, src => src.MapFrom(s => s.Name))
+                    .ForMember(des => des.InstructorDescription, src => src.MapFrom(s => s.Description))
+                    .ForMember(des => des.InstructorAvatar, src => src.MapFrom(s => s.Thumbnail));
+                cfg.CreateMap<Course, CourseDTO>()
+                    .ForMember(des => des.CourseId, src => src.MapFrom(s => s.Id))
+                    .ForMember(des => des.CourseTitle, src => src.MapFrom(s => s.Title))
+                    .ForMember(des => des.CourseDescription, src => src.MapFrom(s => s.Description))
+                    .ForMember(des => des.MarqueeImageUrl, src => src.MapFrom(s => s.MarqueeImageUrl))
+                    .ForMember(des => des.CourseImageUrl, src => src.MapFrom(s => s.ImageUrl));
+            });
+
+            var mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
