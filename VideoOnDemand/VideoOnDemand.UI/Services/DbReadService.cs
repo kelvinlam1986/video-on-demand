1e83ff10cd066d3a47e8e209f899e81e93143166
Minh Lam <kelvincoder@gmail.com>
2018-11-12 23:36:57 +0700

    Add DbReadService class

---
+++ VideoOnDemand/VideoOnDemand.UI/Services/DbReadService.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using VideoOnDemand.Data.Data;
+
+namespace VideoOnDemand.UI.Services
+{
+    public class DbReadService : IDbReadService
+    {
+        private VODContext _db;
+
+        public DbReadService(VODContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<TEntity> Get<TEntity>() where TEntity : class
+        {
+            return _db.Set<TEntity>();
+        }
+
+        public TEntity Get<TEntity>(int id, bool includeRelatedEntities = false) where TEntity : class
+        {
+            var record = _db.Set<TEntity>().Find(new object[] { id });
+            if (record != null && includeRelatedEntities)
+            {
+                var entities = GetEntityNames<TEntity>();
+                foreach (var entity in entities.collections)
+                {
+                    _db.Entry(record).Collection(entity).Load();
+                }
+
+                foreach (var entity in entities.references)
+                {
+                    _db.Entry(record).Reference(entity).Load();
+                }
+            }
+
+            return record;
+        }
+
+        public TEntity Get<TEntity>(string userId, int id) where TEntity : class
+        {
+            var record = _db.Set<TEntity>().Find(new object[] { userId, id });
+            return record;
+        }
+
+        public SelectList GetSelectList<TEntity>(string valueField, string textField) where TEntity : class
+        {
+            var items = Get<TEntity>();
+            return new SelectList(items, valueField, textField);
+        }
+
+        public IEnumerable<TEntity> GetWithIncludes<TEntity>() where TEntity : class
+        {
+            var entityNames = GetEntityNames<TEntity>();
+            var dbset = _db.Set<TEntity>();
+            var entities = entityNames.collections.Union(
+                entityNames.references);
+            foreach (var entity in entities)
+            {
+                _db.Set<TEntity>().Include(entity).Load();
+            }
+
+            return dbset;
+        }
+
+        private (IEnumerable<string> collections, IEnumerable<string> references)
+            GetEntityNames<TEntity>() where TEntity: class
+        {
+            var dbsets = typeof(VODContext)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => x.PropertyType.Name.Contains("DbSet"))
+                .Select(x => x.Name);
+
+            var properties = typeof(TEntity)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            var collections = properties
+                .Where(x => dbsets.Contains(x.Name))
+                .Select(x => x.Name);
+
+            var classes = properties
+                .Where(x => dbsets.Contains(x.Name + "s"))
+                .Select(x => x.Name);
+
+            return (collections: collections, references: classes);
+        }
+    }
+}
