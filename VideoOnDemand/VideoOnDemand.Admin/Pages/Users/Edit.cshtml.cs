13e72dac208281cdbe87af3b0072739323386996
Minh Lam <kelvincoder@gmail.com>
2018-12-10 23:19:00 +0700

    Edit Page and Delete Page

---
+++ VideoOnDemand/VideoOnDemand.Admin/Pages/Users/Edit.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using VideoOnDemand.Admin.Models;
+using VideoOnDemand.Admin.Services;
+
+namespace VideoOnDemand.Admin.Pages.Users
+{
+    [Authorize(Roles = "Admin")]
+    public class EditModel : PageModel
+    {
+        private IUserService _userService;
+
+        [BindProperty]
+        public UserPageModel Input { get; set; } = new UserPageModel();
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public EditModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public void OnGet(string userId)
+        {
+            StatusMessage = string.Empty;
+            Input = _userService.GetUser(userId);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.UpdateUser(Input);
+                if (result)
+                {
+                    StatusMessage = $"User {Input.Email} was updated.";
+                    return RedirectToPage("Index");
+                }
+            }
+
+            // Something failed, redisplay the form.
+            return Page();
+        }
+    }
+}
