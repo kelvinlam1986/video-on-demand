4a06fdda0df41a40dfbe7d69e7b234e7edb4a0f5
Minh Lam <kelvincoder@gmail.com>
2018-12-18 22:44:55 +0700

    Add alert

---
+++ VideoOnDemand/VideoOnDemand.Admin/TagHelpers/StatusMessageTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace VideoOnDemand.Admin.TagHelpers
+{
+    [HtmlTargetElement("status-message")]
+    public class StatusMessageTagHelper : TagHelper
+    {
+        public string Message { get; set; }
+
+        // Possible Bootstrap classes: success, danger, warning, ...
+        public string MessageType { get; set; } = "success";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (string.IsNullOrEmpty(Message))
+            {
+                return;
+            }
+
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            base.Process(context, output);
+
+            output.TagName = "div";
+            var content = $"<div class='alert alert-{MessageType} " +
+               "alert-dismissible' role='alert'><button type='button' " +
+               "class='close' data-dismiss='alert' aria-label='Close'>" +
+               "<span aria-hidden='true'>&times;</span></button>";
+            if (!string.IsNullOrEmpty(Message))
+            {
+                content += Message;
+            }
+
+            output.Content.AppendHtml(content);
+            output.Content.AppendHtml("</div>");
+        }
+    }
+}
