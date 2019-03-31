5ded74ba9769df5f934ce5d2ff4aaa1c098b77d2
Minh Lam <kelvincoder@gmail.com>
2019-03-31 16:13:56 +0700

    Implement download page

---
+++ VideoOnDemand/VideoOnDemand.Admin/Pages/Downloads/Create.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using VideoOnDemand.Data.Data.Entities;
+using VideoOnDemand.Data.Services;
+
+namespace VideoOnDemand.Admin.Pages.Downloads
+{
+    [Authorize(Roles = "Admin")]
+    public class CreateModel : PageModel
+    {
+        private IDbReadService _dbReadService;
+        private IDbWriteService _dbWriteService;
+
+        [BindProperty]
+        public Video Input { get; set; } = new Video();
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public CreateModel(IDbReadService dbReadService, IDbWriteService dbWriteService)
+        {
+            _dbReadService = dbReadService;
+            _dbWriteService = dbWriteService;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Modules"] = _dbReadService.GetSelectList<Module>("Id", "Title");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                Input.CourseId = _dbReadService.Get<Module>(Input.ModuleId).CourseId;
+                var success = await _dbWriteService.Add(Input);
+                if (success)
+                {
+                    StatusMessage = $"Created a new Video: {Input.Title}.";
+                    return RedirectToPage("Index");
+                }
+            }
+
+            // Something failed, redisplay the form.
+            return Page();
+        }
+    }
+}
---
+++ VideoOnDemand/VideoOnDemand.Admin/Pages/Downloads/Delete.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using VideoOnDemand.Data.Data.Entities;
+using VideoOnDemand.Data.Services;
+
+namespace VideoOnDemand.Admin.Pages.Downloads
+{
+    [Authorize(Roles = "Admin")]
+    public class DeleteModel : PageModel
+    {
+        private IDbReadService _dbReadService;
+        private IDbWriteService _dbWriteService;
+
+        [BindProperty]
+        public Video Input { get; set; } = new Video();
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public DeleteModel(IDbReadService dbReadService, IDbWriteService dbWriteService)
+        {
+            _dbReadService = dbReadService;
+            _dbWriteService = dbWriteService;
+        }
+
+        public void OnGet(int id)
+        {
+            Input = _dbReadService.Get<Video>(id, true);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var success = await _dbWriteService.Delete(Input);
+                if (success)
+                {
+                    StatusMessage = $"Deleted Video: {Input.Title}.";
+                    return RedirectToPage("Index");
+                }
+            }
+
+            // Something failed, redisplay the form.
+            return Page();
+        }
+    }
+}
